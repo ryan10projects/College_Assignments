bd26e59a81d0b8ad2ec9be6bbdc83536f18d7f32
ryan10projects <59692861+ryan10projects@users.noreply.github.com>
2021-09-26 20:35:39 +0530

    Add files via upload

---
+++ assignment 4/student_object/validate.js
@@ -0,0 +1,8 @@
+const phone = document.forms["myForm"]["tagline"].value,
+var phoneno = /^\d{10}$/;
+    if(!values.match(phoneno))
+    {
+        alert('Phone number must have 10 digits only');
+        
+    }
+	
