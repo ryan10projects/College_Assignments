159b15eb2b0ea4db0fd7b3f4e61051887f9f44e0
ryan10projects <59692861+ryan10projects@users.noreply.github.com>
2021-09-26 20:37:55 +0530

    Add files via upload

---
+++ assignment 4/student_object/js/student.js
@@ -0,0 +1,42 @@
+function Error() {
+	
+    var value = document.getElementById("Error").value;
+    console.log(value);
+
+    if (isNaN(value) || value == 0) {
+		message.innerHTML = "Input Invalid";
+        throw new Error("Invalid Input");
+    } 
+    else {
+        console.log("Valid Input");
+    }
+}
+
+
+const student1 = {
+    name: "Ryan Rodricks",
+    className: "D15A",
+    rollNo: 0,
+};
+
+console.log("Student 1: ", student1);
+
+class Student {
+    constructor(uname, className, rollNo) {
+        this.uname = uname;
+        this.className = className;
+        this.rollNo = rollNo;
+    }
+}
+
+const student2 = new Student("Ryan Rodricks", "D15A", 0);
+console.log("Student 2: ", student2);
+
+Student.prototype.college = "VESIT";
+console.log("Student 2: ", student2);
+
+document.getElementById("printName").innerHTML = student2.uname;
+document.getElementById("printClass").innerHTML = student2.className;
+document.getElementById("printRoll").innerHTML = student2.rollNo;
+
+
