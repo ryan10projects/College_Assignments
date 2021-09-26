34dd6e18b275187c98b1991140febde9bfccea68
ryan10projects <59692861+ryan10projects@users.noreply.github.com>
2021-09-26 19:28:19 +0530

    Update validate.js

--- assignment 4/js/validate.js
+++ assignment 4/js/validate.js
@@ -1,6 +1,6 @@
-const phone = document.getElementById("phone").innerHTML.value;
+var phone = document.getElementById("phone").innerHTML.value;
 var phoneno = /^\d{10}$/;
-    if(!values.match(phoneno))
+    if(!phone.match(phoneno))
     {
         alert('Phone number must have 10 digits only');
         
