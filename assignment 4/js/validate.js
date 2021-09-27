6519520c305a44e67fa4786c6a52356d67d22e19
ryan10projects <59692861+ryan10projects@users.noreply.github.com>
2021-09-27 18:21:48 +0530

    Update validate.js

--- assignment 4/js/validate.js
+++ assignment 4/js/validate.js
@@ -6,5 +6,11 @@ function validate(number)
       alert("Please enter a valid phone number.");
      return false;
      }
-  
+    var add = document.getElementbyid(address).value;
+
+    if(add==null){
+
+     alert("Please enter address before placing order");
+
+     }
 }  
