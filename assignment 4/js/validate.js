e3e9c8ac611346aa419a6b2258beda8a0b2b9671
ryan10projects <59692861+ryan10projects@users.noreply.github.com>
2021-09-27 23:00:12 +0530

    Update validate.js

--- assignment 4/js/validate.js
+++ assignment 4/js/validate.js
@@ -13,7 +13,7 @@ function validate(number,address)
        
        }
    else {
-      alert("Form submitted");
+      alert("Form submitted"+address);
       }
 
     if(address===null || address==" "){
