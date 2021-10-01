48a1f23ec91f939d35269ca00f519dab539cab96
ryan10projects <59692861+ryan10projects@users.noreply.github.com>
2021-10-01 22:55:40 +0530

    Update student.js

--- assignment 4/student_object/js/student.js
+++ assignment 4/student_object/js/student.js
@@ -1,7 +1,6 @@
 function Error() {
 	
     var value = document.getElementById("Error").value;
-    console.log(value);
 
     if (isNaN(value) || value == 0) {
 		message.innerHTML = "Input Invalid";
@@ -19,8 +18,6 @@ const student1 = {
     rollNo: 60,
 };
 
-console.log("Student details: ", student1);
-
 class Student {
     constructor(name, className, rollNo) {
         this.name = name;
@@ -30,7 +27,8 @@ class Student {
 }
 
 const student2 = new Student("Ryan Rodricks", "D15A", 60);
-console.log("Student 2: ", student2);
+
+
 
 Student.prototype.surname = "Rodricks";
 console.log("Student 2: ", student2);
